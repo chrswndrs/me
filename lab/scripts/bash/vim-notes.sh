dd44de12fdcfe34c3622a6c793daf39935ccb177
chrswndrs <chrswndrs@proton.me>
2022-08-23 22:50:59 +0200

    added vim-notes to lab

---
+++ lab/scripts/bash/vim-notes.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+noteFilename="/Users/christophwanders/Journal/note-$(date +%Y-%m-%d).md"
+
+if [ ! -f $noteFilename ]; then
+  echo "# Notes for $(date +%Y-%m-%d)" > $noteFilename
+fi
+
+vim -c "norm Go" \
+  -c "norm Go## $(date +%H:%M)" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $noteFilename
+
+kill -9 $PPID                        # this will kill the old terminal
+exit       
